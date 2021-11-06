910aad782039dcb79d33dce81caf54a89ca55bb8
Elias Koromilas <elias.koromilas@gmail.com>
2021-11-06 19:13:30 +0200

    [GitHub] Search the entire history of the repository

--- api/index.py
+++ api/index.py
@@ -1,6 +1,7 @@
 from flask import Flask, redirect, request
 
 import fnmatch
+import itertools
 import requests
 
 app = Flask(__name__)
@@ -15,38 +16,45 @@ def view_func():
         if not '/' in repo:
             repo = 'inaccel/' + repo
 
-        release = None
         if 'tag' in params:
-            response = requests.get('https://api.github.com/repos/' + repo +
-                                    '/releases')
+            for page in itertools.count(1):
+                response = requests.get(
+                    'https://api.github.com/repos/{}/releases?page={}&per_page=100'
+                    .format(repo, page))
 
-            if response.status_code == 200:
-                releases = response.json()
-
-                def latest(releases, tag):
-                    for release in releases:
-                        if fnmatch.fnmatch(release['tag_name'], tag):
-                            return release
-
-                release = latest(releases, params.get('tag'))
+                if response.status_code != 200:
+                    return '', response.status_code
+                elif response.json():
+                    for release in response.json():
+                        if fnmatch.fnmatch(release['tag_name'],
+                                           params.get('tag')):
+                            break
+                    else:
+                        continue
+                    break
+                else:
+                    return '', 404  # Not Found
         else:
-            response = requests.get('https://api.github.com/repos/' + repo +
-                                    '/releases/latest')
+            response = requests.get(
+                'https://api.github.com/repos/{}/releases/latest'.format(repo))
 
-            if response.status_code == 200:
+            if response.status_code != 200:
+                return '', response.status_code
+            elif response.json():
                 release = response.json()
+            else:
+                return '', 404  # Not Found
 
-        if release:
-            if 'asset' in params:
-                for asset in release['assets']:
-                    if asset['name'] == params.get('asset'):
-                        return redirect(asset['browser_download_url'])
-            elif 'zip' in params:
-                return redirect(release['zipball_url'])
-            elif 'tar' in params:
-                return redirect(release['tarball_url'])
-            elif release['assets']:
-                return redirect(release['assets'][0]['browser_download_url'])
+        if 'asset' in params:
+            for asset in release['assets']:
+                if asset['name'] == params.get('asset'):
+                    return redirect(asset['browser_download_url'])
+        elif 'zip' in params:
+            return redirect(release['zipball_url'])
+        elif 'tar' in params:
+            return redirect(release['tarball_url'])
+        elif release['assets']:
+            return redirect(release['assets'][0]['browser_download_url'])
 
         return '', 404  # Not Found
     else:
